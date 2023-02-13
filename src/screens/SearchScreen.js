f7f0afe3d06531465be59c4c5915b787191e17f6
Panagiotis Doukas <doukas@cloudevo.ai>
2023-02-13 19:09:46 +0200

    Add the error handling in searchApi

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,16 +6,26 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
+  /*Everytime you make an api request or any type of 
+    network request, remember that is an asyncrhonous
+    operation. So you need to add in a promise, to handle
+    the search results that you will eventually get back.
+  */
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose'
-      }
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: term,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
   };
 
   return (
@@ -25,7 +35,7 @@ const SearchScreen = () => {
         onTermChange={(newTerm) => setTerm(newTerm)} 
         onTermSubmit={() => searchApi()} 
       />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
