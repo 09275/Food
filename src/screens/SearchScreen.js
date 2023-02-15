27565761faf2ed78efb2c467d80195fcc3e5f02a
Panagiotis Doukas <doukas@cloudevo.ai>
2023-02-15 10:06:00 +0200

    Complete the SearchScreen. It will include both the component part and the logic with hooks

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -11,8 +11,7 @@ const SearchScreen = () => {
   /*Everytime you make an api request or any type of 
     network request, remember that is an asyncrhonous
     operation. So you need to add in a promise, to handle
-    the search results that you will eventually get back.
-  */
+    the search results that you will eventually get back */
   const searchApi = async (searchTerm) => {
     try {
       const response = await yelp.get('/search', {
@@ -37,8 +36,7 @@ const SearchScreen = () => {
         ** useEffect(() => {}, [])
     #--Run the arrow function only when the component is first rendered,
        and when the 'value' changes--#
-        ** useEffect(() => {}, [value])
-  */
+        ** useEffect(() => {}, [value]) */
   useEffect(() => {
     searchApi('pasta');
   }, []);
