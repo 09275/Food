93a0600778bc4f86ba18850618614e8fcc8f7558
Panagiotis Doukas <doukas@cloudevo.ai>
2023-02-20 11:57:33 +0200

    Add the ResultsList component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
-  
+
+  /*The filter() method creates a shallow copy of a portion of a given array, 
+    filtered down to just the elements from the given array that pass the test 
+    implemented by the provided function.
+    EXAMPLE:
+    const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+    const result = words.filter(word => word.length > 6);
+    console.log(result);
+    // Expected output: Array ["exuberant", "destruction", "present"] */
+  const filterResultsByPrice = (price) => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter(result => {
+      return result.price === price;
+    });
+  };
 
   return (
     <View>
@@ -17,6 +32,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
     </View>
   );
 };
